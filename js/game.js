153a8eec84892a69dc58033434266fc699612757
Winnypohh <maik523@hotmail.com>
2021-08-06 08:17:23 +0000

    Makeing changes on result box

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@ const choices = document.querySelectorAll('.choice');
 const score = document.getElementById('score');
 const result = document.getElementById('result');
 const restart = document.getElementById('restart');
-const modal = document.querySelector('.modal');
+
 const scoreboard = {
      player: 0,
      computer: 0,
